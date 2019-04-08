126c730e337b5dd304e8aa196f149088483b5919
Rick Riehle <rickr@bitbamboo.com>
2019-04-08 15:44:33 -0700

    blogroll links

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Larry Fritts', 'https://larryfritts.com/'),
+         ('Richard Riehle', 'https://rriehle.com/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -32,4 +30,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
